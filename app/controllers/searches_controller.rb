ed333ee92f6a40ff485a8ac778083df3156ada40
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-08-08 20:29:55 +0000

    automatically backed up by learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -10,7 +10,7 @@ class SearchesController < ApplicationController
       req.params['near'] = params[:zipcode]
       req.params['query'] = 'coffee shop'
     end
- 
+
     body_hash = JSON.parse(@resp.body)
     @venues = body_hash["response"]["venues"]
     render 'search'
